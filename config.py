cfae05b10460f3ef8f3e28a08433d0cd357d652a
Kunal <shah.kunal42@gmail.com>
2021-08-17 00:07:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-client_id = ""
-client_secret = ""
-user_agent = ""
+client_id = "0Ow4cqQ2unehlxZoVDzHEg"
+client_secret = "EVQiN_nA-91JbrZYudsAbZ6moCnIXQ"
+user_agent = "chrome"
 
 post_type = "hot" # hot, top, rising, new
-subreddits = ['memes', 'funny']
+subreddits = ['IndianDankMemes']
 
 # If true, the bot comments to the post in Reddit before reposting it. Some subreddit mods may not be okay with reposting their content.
 comment_submission = False 
@@ -12,8 +12,8 @@ submission_comment = "Hey! I'm a bot and I reposted your post to my Instagram ac
 # DO NOT USE YOUR MAIN INSTAGRAM ACCOUNT, 
 # BECAUSE WHAT THIS BOT DOES IS AGAINST INSTAGRAM'S RULES.
 # YOUR ACCOUNT MAY GET BANNED, SO USE ANOTHER ACCOUNT!
-instagram_username = ""
-instagram_password = ""
-post_caption = "\"{title}\"\nThis post is posted by {author} at {subreddit}.\nScore: {score}"
+instagram_username = "lenduk_"
+instagram_password = "kK#12321"
+post_caption = "{title}"
 
 wait_before_posting = 1800 # In seconds (by default it posts every 30 minute)
